4c940406ef94b14785ad166eb046c599d7ed5997
RasulNur <rasulnuriyev2004@gmail.com>
2024-08-07 11:38:00 +0500

    fix: error

--- src/components/pages/categories/Catalog.tsx
+++ src/components/pages/categories/Catalog.tsx
@@ -5,7 +5,7 @@ export default function Catalog({ categoriesTree }: ICatalogProps) {
     return (
         <div className="grid 2xl:grid-cols-6 xl:grid-cols-5 md:grid-cols-4 grid-cols-3 max-[560px]:grid-cols-2 gap-5">
             {categoriesTree.data.map((category) => {
-                return <CategoryCard category={category} />;
+                return <CategoryCard key={category.id} category={category} />;
             })}
         </div>
     );
